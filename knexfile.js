034f746fbe14ed2b4e1e0fdca287e06ec369d580
kamaboko1 <github65@hotmail.com>
2019-06-17 11:58:20 -0700

    created users table for db

--- knexfile.js
+++ knexfile.js
@@ -1,15 +1,15 @@
 // Update with your config settings.
 
 module.exports = {
-  
+
   development: {
     client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/webauth1'
     },
     migrations: {
-      directory: './data/migrations'
+      directory: './database/migrations'
     },
     pool: {
       afterCreate: (connection, done) => {
