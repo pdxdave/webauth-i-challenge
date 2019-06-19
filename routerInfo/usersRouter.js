d32d2c1583b04589e4cd2d55136af25e1e868cef
kamaboko1 <github65@hotmail.com>
2019-06-18 19:13:43 -0700

    restricted is working with cookies and sessions

--- routerInfo/usersRouter.js
+++ routerInfo/usersRouter.js
@@ -36,6 +36,9 @@ router.post('/login', (req, res) => {
      .first()
      .then(user => {
          if(user && bcrypt.compareSync(password, user.password)){
+             req.session.username = user.username /* only successful logins with this username will be added
+                                                     req.session.username sent to restricted middleware */
+                                                     console.log(req.session.username)
              res.status(200).json({ message: `Welcome ${user.username}!`})
          } else {
              res.status(401).json({ message: 'Invalid Credentials'})
@@ -53,28 +56,39 @@ router.get('/users', restricted, (req, res) => {
     .catch(err => res.send(err))
 })
 
-// Restrited Middleware
-function restricted (req, res, next) {
-    const {username, password} = req.headers;
-
-    if(username && password) {
-
-        Users.findBy({username})
-        .first()
-        .then(user => {
-            console.log('restrict', user)
-            if(user && bcrypt.compareSync(password, user.password)){
-                res.status(200).json({ message: `Welcome ${user.username}!`})
-            } else {
-                res.status(401).json({ message: 'Invalid Credentials'})
-            }
-        })
-        .catch(error => {
-            res.status(500).json(error)
-        });
-   } else {
-    res.status(400).json({ message: 'Please provide credentials'})
-  }
+// Restricted Middleware
+function restricted (req, res, next){
+    if(req.session && req.session.username) { /* If there is a session go to next */
+        next();
+    } else {
+        res.status(401).json({ message: "You do not get to pass"})
+    }
 }
+
+
+// LOGOUT
+
+
+// function restricted (req, res, next) {
+//     const {username, password} = req.headers;
+
+//     if(username && password) {
+
+//         Users.findBy({username})
+//         .first()
+//         .then(user => {
+//             if(user && bcrypt.compareSync(password, user.password)){
+//                 res.status(200).json({ message: `Welcome ${user.username}!`})
+//             } else {
+//                 res.status(401).json({ message: 'Invalid Credentials'})
+//             }
+//         })
+//         .catch(error => {
+//             res.status(500).json(error)
+//         });
+//    } else {
+//     res.status(400).json({ message: 'Please provide credentials'})
+//   }
+// }
 
 module.exports = router;
